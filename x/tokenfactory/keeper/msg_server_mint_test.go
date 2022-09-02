2f8d850127c1b9b32e91e06f9e3d6e6d41cd74e1
Denis Fadeev <denis@fadeev.org>
2022-09-02 15:44:19 +0400

    Added msg_server_mint_test.go

---
+++ x/tokenfactory/keeper/msg_server_mint_test.go
@@ -0,0 +1,44 @@
+package keeper_test
+
+import (
+	"testing"
+
+	"noble/testutil/sample"
+
+	_ "github.com/stretchr/testify/require"
+
+	"noble/x/tokenfactory/types"
+
+	testkeeper "noble/testutil/keeper"
+
+	sdkmath "cosmossdk.io/math"
+	sdk "github.com/cosmos/cosmos-sdk/types"
+)
+
+func TestMsgMint(t *testing.T) {
+	var (
+		sdkCtx, _, ts = testkeeper.NewTestSetup(t)
+		ctx           = sdk.WrapSDKContext(sdkCtx)
+		minter        = sample.Address(r)
+		reciever      = sample.Address(r)
+		amount        = sdk.NewCoin("usdc", sdkmath.NewInt(10000))
+	)
+	for _, tc := range []struct {
+		name string
+		msg  types.MsgMint
+		err  error
+	}{
+		{
+			name: "mint tokens",
+			msg: types.MsgMint{
+				From:    minter,
+				Amount:  amount,
+				Address: reciever,
+			},
+		},
+	} {
+		t.Run(tc.name, func(t *testing.T) {
+			ts.TokenfactorySrv.Mint(ctx, &tc.msg)
+		})
+	}
+}
--- x/tokenfactory/keeper/msg_server_test.go
+++ x/tokenfactory/keeper/msg_server_test.go
@@ -2,47 +2,16 @@ package keeper_test
 
 import (
 	"context"
+	"noble/x/tokenfactory/keeper"
+	"noble/x/tokenfactory/types"
 	"testing"
 
 	keepertest "noble/testutil/keeper"
-	"noble/testutil/sample"
-	"noble/x/tokenfactory/keeper"
 
-	_ "github.com/stretchr/testify/require"
-
-	"noble/x/tokenfactory/types"
-
-	testkeeper "noble/testutil/keeper"
-
-	sdkmath "cosmossdk.io/math"
 	sdk "github.com/cosmos/cosmos-sdk/types"
 )
 
-func setupMsgServer(t *testing.T) (types.MsgServer, context.Context) {
-	var (
-		_, _, ts = testkeeper.NewTestSetup(t)
-		minter   = sample.Address(r)
-		reciever = sample.Address(r)
-		amount   = sdk.NewCoin("usdc", sdkmath.NewInt(10000))
-	)
+func setupMsgServer(t testing.TB) (types.MsgServer, context.Context) {
 	k, ctx := keepertest.TokenfactoryKeeper(t)
-	for _, tc := range []struct {
-		name string
-		msg  types.MsgMint
-		err  error
-	}{
-		{
-			name: "mint tokens",
-			msg: types.MsgMint{
-				From:    minter,
-				Amount:  amount,
-				Address: reciever,
-			},
-		},
-	} {
-		t.Run(tc.name, func(t *testing.T) {
-			ts.TokenfactorySrv.Mint(ctx, &tc.msg)
-		})
-	}
 	return keeper.NewMsgServerImpl(*k), sdk.WrapSDKContext(ctx)
 }
