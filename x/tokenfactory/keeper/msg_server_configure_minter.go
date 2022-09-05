262bfa2e8add4ff0540c7c62bf8e1976d7fe0f7e
Denis Fadeev <denis@fadeev.org>
2022-09-05 10:44:24 +0400

    ConfigureMinter logic

--- x/tokenfactory/keeper/msg_server_configure_minter.go
+++ x/tokenfactory/keeper/msg_server_configure_minter.go
@@ -3,15 +3,32 @@ package keeper
 import (
 	"context"
 
-	sdk "github.com/cosmos/cosmos-sdk/types"
 	"noble/x/tokenfactory/types"
+
+	sdkerrors "cosmossdk.io/errors"
+	sdk "github.com/cosmos/cosmos-sdk/types"
 )
 
 func (k msgServer) ConfigureMinter(goCtx context.Context, msg *types.MsgConfigureMinter) (*types.MsgConfigureMinterResponse, error) {
 	ctx := sdk.UnwrapSDKContext(goCtx)
 
-	// TODO: Handling the message
-	_ = ctx
+	owner, found := k.GetMasterMinter(ctx)
+	if !found {
+		return nil, sdkerrors.Wrapf(types.ErrUserNotFound, "master minter isn't set")
+	}
+
+	if owner.Address != msg.From {
+		return nil, sdkerrors.Wrapf(types.ErrUnauthorized, "you are not the master minter")
+	}
+
+	// TODO: https://github.com/strangelove-ventures/noble/issues/4
+
+	minter := types.Minters{
+		Address:   msg.Address,
+		Allowance: msg.Allowance,
+	}
+
+	k.SetMinters(ctx, minter)
 
 	return &types.MsgConfigureMinterResponse{}, nil
 }
