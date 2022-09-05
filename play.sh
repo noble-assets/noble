d036f84ae4fe38deebff9c9508c8b71e490a1ed5
Denis Fadeev <denis@fadeev.org>
2022-09-05 11:50:20 +0400

    Update play.sh

--- play.sh
+++ play.sh
@@ -1,16 +1,32 @@
-export MASTER_MINTER_ADDRESS=$(nobled keys show masterminter -a)
-export MINTER_ADDRESS=$(nobled keys show minter -a)
-export USER_ADDRESS=$(nobled keys show user -a)
-
-sleep 3
-nobled tx tokenfactory update-master-minter $MASTER_MINTER_ADDRESS --from owner -y
-sleep 3
-nobled tx tokenfactory configure-minter $MINTER_ADDRESS 1000usdc --from masterminter -y
-sleep 3
-nobled tx tokenfactory mint $USER_ADDRESS 100usdc --from minter -y
-sleep 3
-nobled q bank balances $USER_ADDRESS
-sleep 3
+sleep 2
+nobled tx tokenfactory update-master-minter $(nobled keys show masterminter -a) --from owner -y
+sleep 2
+nobled tx tokenfactory configure-minter $(nobled keys show minter -a) 1000usdc --from masterminter -y
+sleep 2
+nobled tx tokenfactory mint $(nobled keys show user -a) 100usdc --from minter -y
+sleep 2
+nobled q bank balances $(nobled keys show user -a)
+sleep 2
 nobled q tokenfactory list-minters
-sleep 3
-nobled tx tokenfactory mint $USER_ADDRESS 99999999999999usdc --from minter -y
+sleep 2
+nobled tx tokenfactory mint $(nobled keys show user -a) 99999999999999usdc --from minter -y
+sleep 2
+nobled tx tokenfactory update-blacklister $(nobled keys show blacklister -a) --from owner -y
+sleep 2
+nobled tx tokenfactory blacklist $(nobled keys show minter -a) --from blacklister -y
+sleep 2
+nobled tx tokenfactory mint $(nobled keys show user -a) 100usdc --from minter -y
+sleep 2
+nobled tx tokenfactory unblacklist $(nobled keys show minter -a) --from blacklister -y
+sleep 2
+nobled tx tokenfactory mint $(nobled keys show user -a) 100usdc --from minter -y
+sleep 2
+nobled q bank balances $(nobled keys show user -a)
+sleep 2
+nobled tx tokenfactory blacklist $(nobled keys show user -a) --from blacklister -y
+sleep 2
+nobled tx tokenfactory mint $(nobled keys show user -a) 100usdc --from minter -y
+sleep 2
+nobled tx tokenfactory unblacklist $(nobled keys show user -a) --from blacklister -y
+sleep 2
+nobled tx tokenfactory mint $(nobled keys show user -a) 100usdc --from minter -y
