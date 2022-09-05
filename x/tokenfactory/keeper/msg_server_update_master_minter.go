8fa37a86056eee3f25da6bdd4fac0e91638dd4bd
Denis Fadeev <denis@fadeev.org>
2022-09-05 10:18:00 +0400

    UpdateMasterMinter logic

--- x/tokenfactory/keeper/msg_server_update_master_minter.go
+++ x/tokenfactory/keeper/msg_server_update_master_minter.go
@@ -3,15 +3,29 @@ package keeper
 import (
 	"context"
 
-	sdk "github.com/cosmos/cosmos-sdk/types"
 	"noble/x/tokenfactory/types"
+
+	sdkerrors "cosmossdk.io/errors"
+	sdk "github.com/cosmos/cosmos-sdk/types"
 )
 
 func (k msgServer) UpdateMasterMinter(goCtx context.Context, msg *types.MsgUpdateMasterMinter) (*types.MsgUpdateMasterMinterResponse, error) {
 	ctx := sdk.UnwrapSDKContext(goCtx)
 
-	// TODO: Handling the message
-	_ = ctx
+	owner, found := k.GetOwner(ctx)
+	if !found {
+		return nil, sdkerrors.Wrapf(types.ErrUserNotFound, "owner isn't set")
+	}
+
+	if owner.Address != msg.From {
+		return nil, sdkerrors.Wrapf(types.ErrUnauthorized, "you are not the owner")
+	}
+
+	masterMinter := types.MasterMinter{
+		Address: msg.Address,
+	}
+
+	k.SetMasterMinter(ctx, masterMinter)
 
 	return &types.MsgUpdateMasterMinterResponse{}, nil
 }
