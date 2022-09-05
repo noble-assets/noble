515b449b871f61eda7ddabb246f6116d931755d4
Denis Fadeev <denis@fadeev.org>
2022-09-05 10:52:53 +0400

    RemoveMinter and Blacklist logic

--- x/tokenfactory/keeper/msg_server_remove_minter.go
+++ x/tokenfactory/keeper/msg_server_remove_minter.go
@@ -3,15 +3,30 @@ package keeper
 import (
 	"context"
 
-	sdk "github.com/cosmos/cosmos-sdk/types"
 	"noble/x/tokenfactory/types"
+
+	sdkerrors "cosmossdk.io/errors"
+	sdk "github.com/cosmos/cosmos-sdk/types"
 )
 
 func (k msgServer) RemoveMinter(goCtx context.Context, msg *types.MsgRemoveMinter) (*types.MsgRemoveMinterResponse, error) {
 	ctx := sdk.UnwrapSDKContext(goCtx)
 
-	// TODO: Handling the message
-	_ = ctx
+	owner, found := k.GetMasterMinter(ctx)
+	if !found {
+		return nil, sdkerrors.Wrapf(types.ErrUserNotFound, "master minter isn't set")
+	}
+
+	if owner.Address != msg.From {
+		return nil, sdkerrors.Wrapf(types.ErrUnauthorized, "you are not the master minter")
+	}
+
+	minter, found := k.GetMinters(ctx, msg.Address)
+	if !found {
+		return nil, sdkerrors.Wrapf(types.ErrUserNotFound, "a minter with a given address doesn't exist")
+	}
+
+	k.RemoveMinters(ctx, minter.Address)
 
 	return &types.MsgRemoveMinterResponse{}, nil
 }
