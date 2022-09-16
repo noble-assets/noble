8a60b848bd197f901bb0065e95c28994566aa658
Denis Fadeev <denis@fadeev.org>
2022-09-16 12:34:12 +0400

    add configure minter controller logic

--- x/tokenfactory/keeper/msg_server_configure_minter_controller.go
+++ x/tokenfactory/keeper/msg_server_configure_minter_controller.go
@@ -3,15 +3,30 @@ package keeper
 import (
 	"context"
 
-	sdk "github.com/cosmos/cosmos-sdk/types"
 	"noble/x/tokenfactory/types"
+
+	sdkerrors "cosmossdk.io/errors"
+	sdk "github.com/cosmos/cosmos-sdk/types"
 )
 
 func (k msgServer) ConfigureMinterController(goCtx context.Context, msg *types.MsgConfigureMinterController) (*types.MsgConfigureMinterControllerResponse, error) {
 	ctx := sdk.UnwrapSDKContext(goCtx)
 
-	// TODO: Handling the message
-	_ = ctx
+	masterMinter, found := k.GetMasterMinter(ctx)
+	if !found {
+		return nil, sdkerrors.Wrapf(types.ErrUserNotFound, "master minter is not set")
+	}
+
+	if masterMinter.Address != msg.From {
+		return nil, sdkerrors.Wrapf(types.ErrUnauthorized, "you are not the master minter")
+	}
+
+	controller := types.MinterController{
+		MinterAddress: msg.Minter,
+		Address:       msg.Controller,
+	}
+
+	k.SetMinterController(ctx, controller)
 
 	return &types.MsgConfigureMinterControllerResponse{}, nil
 }
