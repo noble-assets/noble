af826922d662982bd900da8b868aaec95afe9471
Denis Fadeev <denis@fadeev.org>
2022-10-04 12:45:19 +0400

    refactor: moved genesis values into the genesis file

--- x/tokenfactory/types/genesis.go
+++ x/tokenfactory/types/genesis.go
@@ -10,16 +10,6 @@ const DefaultIndex uint64 = 1
 // DefaultGenesis returns the default genesis state
 func DefaultGenesis() *GenesisState {
 	return &GenesisState{
-		BlacklistedList:      []Blacklisted{},
-		Paused:               &Paused{Paused: false},
-		MasterMinter:         nil,
-		MintersList:          []Minters{},
-		Pauser:               nil,
-		Blacklister:          nil,
-		Owner:                &Owner{Address: "cosmos1rde4hcxtztnzdzkta5xergke5yzsf877xzfxdh"},
-		Admin:                &Admin{Address: "cosmos1qqj305p7vhzfcxku8wp7eh64he452nerwlw93k"},
-		MinterControllerList: []MinterController{},
-		MintingDenom:         &MintingDenom{Denom: "usdc"},
 		// this line is used by starport scaffolding # genesis/types/default
 		Params: DefaultParams(),
 	}
