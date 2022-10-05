1940d2e8793df6b9941af9880acb1e6cd0706bb1
Denis Fadeev <denis@fadeev.org>
2022-10-05 15:27:17 +0400

    feat: fail unless admin

--- x/tokenfactory/keeper/msg_server_software_upgrade.go
+++ x/tokenfactory/keeper/msg_server_software_upgrade.go
@@ -5,11 +5,21 @@ import (
 
 	"noble/x/tokenfactory/types"
 
+	sdkerrors "cosmossdk.io/errors"
 	sdk "github.com/cosmos/cosmos-sdk/types"
 )
 
 func (k msgServer) SoftwareUpgrade(goCtx context.Context, msg *types.MsgSoftwareUpgrade) (*types.MsgSoftwareUpgradeResponse, error) {
 	ctx := sdk.UnwrapSDKContext(goCtx)
+
+	admin, found := k.GetAdmin(ctx)
+	if !found {
+		return nil, sdkerrors.Wrapf(types.ErrUserNotFound, "admin is not set")
+	}
+
+	if admin.Address != msg.From {
+		return nil, sdkerrors.Wrapf(types.ErrUnauthorized, "you are not the admin")
+	}
 
 	k.upgradeKeeper.ScheduleUpgrade(ctx, *msg.Plan)
 
