60599d47d1beeb8226eb995dfe721bf216f5f4e8
jtieri <justin@thetieris.com>
2023-02-07 16:37:46 -0600

    chore: future-proof the protocgen script to work with proto files outside of tokenfactory

--- scripts/protocgen.sh
+++ scripts/protocgen.sh
@@ -26,5 +26,5 @@ Mgoogle/protobuf/any.proto=github.com/cosmos/cosmos-sdk/codec/types:. \
 done
 
 # move proto files to the right places
-cp -r github.com/strangelove-ventures/noble/x/tokenfactory/types/* x/tokenfactory/types/
+cp -r github.com/strangelove-ventures/noble/* ./
 rm -rf github.com
