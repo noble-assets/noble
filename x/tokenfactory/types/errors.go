8f52335f9985b629948269741ed4f59180b50e2a
Justin Tieri <37750742+jtieri@users.noreply.github.com>
2023-02-08 16:28:28 -0600

    Validate that a user is not already blacklisted (#92)

--- x/tokenfactory/types/errors.go
+++ x/tokenfactory/types/errors.go
@@ -15,4 +15,5 @@ var (
 	ErrBurn               = sdkerrors.Register(ModuleName, 6, "tokens can not be burned")
 	ErrPaused             = sdkerrors.Register(ModuleName, 7, "the chain is paused")
 	ErrInvalidPubkey      = sdkerrors.Register(ModuleName, 8, "pubkey bytes are invalid")
+	ErrUserBlacklisted    = sdkerrors.Register(ModuleName, 9, "user is already blacklisted")
 )
