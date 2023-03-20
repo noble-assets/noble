2ca73980e7654cf7499df1682d06dcf80e247377
Dan Kanefsky <daniel.kanefsky@gmail.com>
2023-03-20 08:31:51 -0700

    upgrade info

--- app/upgrades/neon/constants.go
+++ app/upgrades/neon/constants.go
@@ -4,5 +4,5 @@ const (
 	// UpgradeName is the shared upgrade plan name for mainnet
 	UpgradeName = "v0.4.1"
 	// UpgradeInfo defines the binaries that will be used for the upgrade
-	UpgradeInfo = "Release tag: https://github.com/strangelove-ventures/noble/releases/tag/v0.4.1"
+	UpgradeInfo = "Release tag: https://github.com/strangelove-ventures/noble/releases/tag/v0.4.2"
 )
