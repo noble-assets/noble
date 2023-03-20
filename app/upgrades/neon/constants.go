18619f61f844ef7d1c2b050c2cdd2d1ec5dc9034
Dan Kanefsky <56059752+boojamya@users.noreply.github.com>
2023-03-20 08:32:48 -0700

    Change `UpgradeName` in upgrade handler to `v0.4.1` (#141)

--- app/upgrades/neon/constants.go
+++ app/upgrades/neon/constants.go
@@ -2,7 +2,7 @@ package neon
 
 const (
 	// UpgradeName is the shared upgrade plan name for mainnet
-	UpgradeName = "neon"
+	UpgradeName = "v0.4.1"
 	// UpgradeInfo defines the binaries that will be used for the upgrade
-	UpgradeInfo = "Release tag: https://github.com/strangelove-ventures/noble/releases/tag/v0.4.1"
+	UpgradeInfo = "Release tag: https://github.com/strangelove-ventures/noble/releases/tag/v0.4.2"
 )
