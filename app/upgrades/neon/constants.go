7d605f3260677d056a2f7ee7bd4b3a5864008bdc
Andrew Gouin <andrew@gouin.io>
2023-03-28 11:50:15 -0600

    Regen protos, update neon upgrade description

--- app/upgrades/neon/constants.go
+++ app/upgrades/neon/constants.go
@@ -1,8 +1,8 @@
 package neon
 
 const (
-	// UpgradeName is the shared upgrade plan name for mainnet
+	// UpgradeName is the shared upgrade plan name
 	UpgradeName = "neon"
-	// UpgradeInfo defines the binaries that will be used for the upgrade
-	UpgradeInfo = "Release tag: https://github.com/strangelove-ventures/noble/releases/tag/v0.4.1"
+	// UpgradeInfo describes the upgrade
+	UpgradeInfo = "Noble Neon upgrade"
 )
