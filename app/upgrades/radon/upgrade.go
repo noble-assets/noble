a1971458875628375884a4c34360a83e148e059d
Dan Kanefsky <daniel.kanefsky@gmail.com>
2023-04-12 13:27:12 -0700

    radon denom metadata patch

--- app/upgrades/radon/upgrade.go
+++ app/upgrades/radon/upgrade.go
@@ -26,15 +26,59 @@ func CreateRadonUpgradeHandler(
 		// becasuse RunMigrations runs `InitGenesis` on new modules`.
 		versionMap, err := mm.RunMigrations(ctx, cfg, vm)
 
+		feeDenom := fiatTFKeeper.GetMintingDenom(ctx)
+
 		// -- globalfee params --
-		minGasPrices := sdk.DecCoins{
-			sdk.NewDecCoinFromDec("udrachma", sdk.NewDecWithPrec(1, 2)),
+		globalFeeParams := globalfeetypes.Params{
+			MinimumGasPrices: sdk.DecCoins{
+				sdk.NewDecCoinFromDec(feeDenom.Denom, sdk.NewDec(0)),
+			},
+			BypassMinFeeMsgTypes: []string{
+				"/ibc.core.client.v1.MsgUpdateClient",
+				"/ibc.core.channel.v1.MsgRecvPacket",
+				"/ibc.core.channel.v1.MsgAcknowledgement",
+				"/ibc.applications.transfer.v1.MsgTransfer",
+				"/ibc.core.channel.v1.MsgTimeout",
+				"/ibc.core.channel.v1.MsgTimeoutOnClose",
+				"/cosmos.params.v1beta1.MsgUpdateParams",
+				"/cosmos.upgrade.v1beta1.MsgSoftwareUpgrade",
+				"/cosmos.upgrade.v1beta1.MsgCancelUpgrade",
+				"/noble.fiattokenfactory.MsgUpdateMasterMinter",
+				"/noble.fiattokenfactory.MsgUpdatePauser",
+				"/noble.fiattokenfactory.MsgUpdateBlacklister",
+				"/noble.fiattokenfactory.MsgUpdateOwner",
+				"/noble.fiattokenfactory.MsgAcceptOwner",
+				"/noble.fiattokenfactory.MsgConfigureMinter",
+				"/noble.fiattokenfactory.MsgRemoveMinter",
+				"/noble.fiattokenfactory.MsgMint",
+				"/noble.fiattokenfactory.MsgBurn",
+				"/noble.fiattokenfactory.MsgBlacklist",
+				"/noble.fiattokenfactory.MsgUnblacklist",
+				"/noble.fiattokenfactory.MsgPause",
+				"/noble.fiattokenfactory.MsgUnpause",
+				"/noble.fiattokenfactory.MsgConfigureMinterController",
+				"/noble.fiattokenfactory.MsgRemoveMinterController",
+				"/noble.tokenfactory.MsgUpdatePauser",
+				"/noble.tokenfactory.MsgUpdateBlacklister",
+				"/noble.tokenfactory.MsgUpdateOwner",
+				"/noble.tokenfactory.MsgAcceptOwner",
+				"/noble.tokenfactory.MsgConfigureMinter",
+				"/noble.tokenfactory.MsgRemoveMinter",
+				"/noble.tokenfactory.MsgMint",
+				"/noble.tokenfactory.MsgBurn",
+				"/noble.tokenfactory.MsgBlacklist",
+				"/noble.tokenfactory.MsgUnblacklist",
+				"/noble.tokenfactory.MsgPause",
+				"/noble.tokenfactory.MsgUnpause",
+				"/noble.tokenfactory.MsgConfigureMinterController",
+				"/noble.tokenfactory.MsgRemoveMinterController",
+			},
 		}
 		globlaFeeParamsSubspace, ok := paramauthoritykeeper.GetSubspace(globalfeetypes.ModuleName)
 		if !ok {
 			panic("global fee params subspace not found")
 		}
-		globlaFeeParamsSubspace.Set(ctx, globalfeetypes.ParamStoreKeyMinGasPrices, minGasPrices)
+		globlaFeeParamsSubspace.SetParamSet(ctx, &globalFeeParams)
 		// -- --
 
 		// -- tariff params --
@@ -49,7 +93,6 @@ func CreateRadonUpgradeHandler(
 				Share:   sdk.NewDec(1),
 			},
 		}
-		feeDenom := fiatTFKeeper.GetMintingDenom(ctx)
 		tariffParams := tarifftypes.Params{
 			Share:                sdk.NewDecWithPrec(8, 1),
 			DistributionEntities: distributionEntities,
