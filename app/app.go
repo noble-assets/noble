6dbef3f7bb32f705d0c11fe0d783766dd6d1fde9
Andrew Gouin <andrew@gouin.io>
2023-04-12 17:01:16 -0600

    bypass min fees hack

--- app/app.go
+++ app/app.go
@@ -164,6 +164,47 @@ var (
 	_ cmd.App                 = (*App)(nil)
 	_ servertypes.Application = (*App)(nil)
 	_ simapp.App              = (*App)(nil)
+
+	defaultBypassMinFeeMsgTypes = []string{
+		"/ibc.core.client.v1.MsgUpdateClient",
+		"/ibc.core.channel.v1.MsgRecvPacket",
+		"/ibc.core.channel.v1.MsgAcknowledgement",
+		"/ibc.applications.transfer.v1.MsgTransfer",
+		"/ibc.core.channel.v1.MsgTimeout",
+		"/ibc.core.channel.v1.MsgTimeoutOnClose",
+		"/cosmos.params.v1beta1.MsgUpdateParams",
+		"/cosmos.upgrade.v1beta1.MsgSoftwareUpgrade",
+		"/cosmos.upgrade.v1beta1.MsgCancelUpgrade",
+		"/noble.fiattokenfactory.MsgUpdateMasterMinter",
+		"/noble.fiattokenfactory.MsgUpdatePauser",
+		"/noble.fiattokenfactory.MsgUpdateBlacklister",
+		"/noble.fiattokenfactory.MsgUpdateOwner",
+		"/noble.fiattokenfactory.MsgAcceptOwner",
+		"/noble.fiattokenfactory.MsgConfigureMinter",
+		"/noble.fiattokenfactory.MsgRemoveMinter",
+		"/noble.fiattokenfactory.MsgMint",
+		"/noble.fiattokenfactory.MsgBurn",
+		"/noble.fiattokenfactory.MsgBlacklist",
+		"/noble.fiattokenfactory.MsgUnblacklist",
+		"/noble.fiattokenfactory.MsgPause",
+		"/noble.fiattokenfactory.MsgUnpause",
+		"/noble.fiattokenfactory.MsgConfigureMinterController",
+		"/noble.fiattokenfactory.MsgRemoveMinterController",
+		"/noble.tokenfactory.MsgUpdatePauser",
+		"/noble.tokenfactory.MsgUpdateBlacklister",
+		"/noble.tokenfactory.MsgUpdateOwner",
+		"/noble.tokenfactory.MsgAcceptOwner",
+		"/noble.tokenfactory.MsgConfigureMinter",
+		"/noble.tokenfactory.MsgRemoveMinter",
+		"/noble.tokenfactory.MsgMint",
+		"/noble.tokenfactory.MsgBurn",
+		"/noble.tokenfactory.MsgBlacklist",
+		"/noble.tokenfactory.MsgUnblacklist",
+		"/noble.tokenfactory.MsgPause",
+		"/noble.tokenfactory.MsgUnpause",
+		"/noble.tokenfactory.MsgConfigureMinterController",
+		"/noble.tokenfactory.MsgRemoveMinterController",
+	}
 )
 
 func init() {
@@ -645,9 +686,10 @@ func New(
 			tokenFactoryKeeper:     app.TokenFactoryKeeper,
 			fiatTokenFactoryKeeper: app.FiatTokenFactoryKeeper,
 
-			IBCKeeper:         app.IBCKeeper,
-			GlobalFeeSubspace: app.GetSubspace(globalfee.ModuleName),
-			StakingSubspace:   app.GetSubspace(stakingtypes.ModuleName),
+			IBCKeeper:            app.IBCKeeper,
+			BypassMinFeeMsgTypes: defaultBypassMinFeeMsgTypes,
+			GlobalFeeSubspace:    app.GetSubspace(globalfee.ModuleName),
+			StakingSubspace:      app.GetSubspace(stakingtypes.ModuleName),
 		},
 	)
 	if err != nil {
