1be67bfd05300584e5be61e98d3fb8d2ba3aa903
Andrew Gouin <andrew@gouin.io>
2023-07-17 22:52:25 -0600

    remove validategenesis that doesn't allow default genesis state

--- x/cctp/types/genesis.go
+++ x/cctp/types/genesis.go
@@ -25,22 +25,11 @@ func DefaultGenesis() *GenesisState {
 // Validate performs basic genesis state validation returning an error upon any
 // failure.
 func (gs GenesisState) Validate() error {
-	var addresses []sdk.AccAddress
-
-	if gs.Authority == nil {
-		return fmt.Errorf("authority cannot be nil")
-	}
-
 	if gs.Authority != nil {
-		owner, err := sdk.AccAddressFromBech32(gs.Authority.Address)
+		_, err := sdk.AccAddressFromBech32(gs.Authority.Address)
 		if err != nil {
 			return sdkerrors.Wrapf(sdkerrors.ErrInvalidAddress, "invalid authority address (%s)", err)
 		}
-		addresses = append(addresses, owner)
-	}
-
-	if gs.PublicKeysList == nil {
-		return fmt.Errorf("PublicKeysList cannot be nil")
 	}
 
 	// Check for duplicated index in public keys
@@ -53,10 +42,6 @@ func (gs GenesisState) Validate() error {
 		publicKeysIndexMap[index] = struct{}{}
 	}
 
-	if gs.MinterAllowanceList == nil {
-		return fmt.Errorf("minter allowance cannot be nil")
-	}
-
 	// Check for duplicated index in minter allowance
 	minterAllowancesIndexMap := make(map[string]struct{})
 	for _, elem := range gs.MinterAllowanceList {
@@ -65,30 +50,6 @@ func (gs GenesisState) Validate() error {
 			return fmt.Errorf("duplicated index for public keys")
 		}
 		minterAllowancesIndexMap[index] = struct{}{}
-	}
-
-	if gs.PerMessageBurnLimit == nil || gs.PerMessageBurnLimit.Amount < 0 {
-		return fmt.Errorf("per message burn limit cannot be nil or less than 0")
-	}
-
-	if gs.MaxMessageBodySize != nil && gs.MaxMessageBodySize.Amount < 0 {
-		return fmt.Errorf("max message body size cannot be less than 0")
-	}
-
-	if gs.BurningAndMintingPaused == nil {
-		return fmt.Errorf("BurningAndMintingPaused cannot be nil")
-	}
-
-	if gs.SendingAndReceivingMessagesPaused == nil {
-		return fmt.Errorf("SendingAndReceivingMessagesPaused cannot be nil")
-	}
-
-	if gs.MaxMessageBodySize == nil {
-		return fmt.Errorf("MaxMessageBodySize cannot be nil")
-	}
-
-	if gs.Nonce == nil {
-		return fmt.Errorf("nonce cannot be nil")
 	}
 
 	return gs.Params.Validate()
