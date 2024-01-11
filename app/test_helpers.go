f7ccaa19d44bbc2851248a3d094d5d8aaeee338c
uwezukwechibuzor <uwezukwechibuzor@gmail.com>
2024-01-11 21:45:45 +0100

    test: improve test setup in app package

---
+++ app/test_helpers.go
@@ -0,0 +1,55 @@
+package app
+
+import (
+	"encoding/json"
+
+	"github.com/cosmos/cosmos-sdk/simapp"
+
+	sdk "github.com/cosmos/cosmos-sdk/types"
+	bankkeeper "github.com/cosmos/cosmos-sdk/x/bank/keeper"
+	minttypes "github.com/cosmos/cosmos-sdk/x/mint/types"
+	abci "github.com/tendermint/tendermint/abci/types"
+	"github.com/tendermint/tendermint/libs/log"
+	dbm "github.com/tendermint/tm-db"
+
+	"github.com/noble-assets/noble/v5/cmd"
+)
+
+// EmptyAppOptions is a stub implementing AppOptions
+type EmptyAppOptions struct{}
+
+// Get implements AppOptions
+func (ao EmptyAppOptions) Get(o string) interface{} {
+	return nil
+}
+
+func Setup(isCheckTx bool) cmd.App {
+	db := dbm.NewMemDB()
+	app := New(log.NewNopLogger(), db, nil, true, map[int64]bool{}, DefaultNodeHome, 5, cmd.MakeEncodingConfig(ModuleBasics), simapp.EmptyAppOptions{})
+	if !isCheckTx {
+		genesisState := NewDefaultGenesisState(cmd.MakeEncodingConfig(ModuleBasics).Marshaler)
+		stateBytes, err := json.MarshalIndent(genesisState, "", " ")
+		if err != nil {
+			panic(err)
+		}
+
+		// Initialize the chain
+		app.InitChain(
+			abci.RequestInitChain{
+				Validators:      []abci.ValidatorUpdate{},
+				ConsensusParams: simapp.DefaultConsensusParams,
+				AppStateBytes:   stateBytes,
+			},
+		)
+	}
+
+	return app
+}
+
+func FundAccount(bankKeeper bankkeeper.Keeper, ctx sdk.Context, addr sdk.AccAddress, amounts sdk.Coins) error {
+	if err := bankKeeper.MintCoins(ctx, minttypes.ModuleName, amounts); err != nil {
+		return err
+	}
+
+	return bankKeeper.SendCoinsFromModuleToAccount(ctx, minttypes.ModuleName, addr, amounts)
+}
