2ecfb90599a07d01404dab32210fb93f2b5d88ec
Aleksandr Bezobchuk <aleks.bezobchuk@gmail.com>
2024-03-20 16:48:23 -0700

    updates

--- x/tariff/module_simulation.go
+++ x/tariff/module_simulation.go
@@ -1,11 +1,10 @@
 package tariff
 
 import (
-	sdk "github.com/cosmos/cosmos-sdk/types"
 	"github.com/cosmos/cosmos-sdk/types/module"
+	"github.com/cosmos/cosmos-sdk/types/simulation"
 	simtypes "github.com/cosmos/cosmos-sdk/types/simulation"
 	"github.com/noble-assets/noble/v5/x/tariff/types"
-	"math/rand"
 )
 
 var _ module.AppModuleSimulation = AppModule{}
@@ -15,15 +14,7 @@ func (am AppModule) GenerateGenesisState(simState *module.SimulationState) {
 	simState.GenState[types.ModuleName] = simState.Cdc.MustMarshalJSON(genesis)
 }
 
-func (am AppModule) ProposalContents(_ module.SimulationState) []simtypes.WeightedProposalContent {
-	return nil
-}
-
-func (am AppModule) RandomizedParams(_ *rand.Rand) []simtypes.ParamChange {
-	return nil
-}
-
-func (am AppModule) RegisterStoreDecoder(_ sdk.StoreDecoderRegistry) {}
+func (am AppModule) RegisterStoreDecoder(_ simulation.StoreDecoderRegistry) {}
 
 func (am AppModule) WeightedOperations(_ module.SimulationState) []simtypes.WeightedOperation {
 	return nil
