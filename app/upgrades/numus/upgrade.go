80ce494b1b38d73ab624009c671594df8957158e
John Letey <john@noble.xyz>
2024-09-13 09:09:15 -0400

    feat: add mainnet owner addresses

--- app/upgrades/numus/upgrade.go
+++ app/upgrades/numus/upgrade.go
@@ -2,6 +2,7 @@ package numus
 
 import (
 	"fmt"
+
 	sdk "github.com/cosmos/cosmos-sdk/types"
 	"github.com/cosmos/cosmos-sdk/types/module"
 	bankkeeper "github.com/cosmos/cosmos-sdk/x/bank/keeper"
@@ -32,8 +33,8 @@ func CreateUpgradeHandler(
 			florinKeeper.SetOwner(ctx, "ueure", "noble1tv9u97jln0k3anpzhahkeahh66u74dug302pyn")
 			florinKeeper.SetBlacklistOwner(ctx, "noble1tv9u97jln0k3anpzhahkeahh66u74dug302pyn")
 		case MainnetChainID:
-			florinKeeper.SetOwner(ctx, "ueure", "") // TODO
-			florinKeeper.SetBlacklistOwner(ctx, "") // TODO
+			florinKeeper.SetOwner(ctx, "ueure", "noble1ya7ggnwv78qcnkv89lte30yge54ztzst3usgmw")
+			florinKeeper.SetBlacklistOwner(ctx, "noble1ya7ggnwv78qcnkv89lte30yge54ztzst3usgmw")
 		default:
 			return vm, fmt.Errorf("%s upgrade not allowed to execute on %s chain", UpgradeName, ctx.ChainID())
 		}
