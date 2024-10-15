6f0536057cc17d8737f0cb26983054bfd919d661
Dan Kanefsky <daniel.kanefsky@gmail.com>
2024-10-15 16:27:06 -0700

    chore: remove chain ID const

--- upgrade/constants.go
+++ upgrade/constants.go
@@ -3,12 +3,6 @@ package upgrade
 // UpgradeName is the name of this specific software upgrade used on-chain.
 const UpgradeName = "helium"
 
-// MainnetChainID is the Chain ID of the Noble mainnet.
-const MainnetChainID = "noble-1"
-
-// TestnetChainID is the Chain ID of the Noble testnet.
-const TestnetChainID = "grand-1"
-
 const UpgradeASCII = `
  _   _      _ _                 
 | | | | ___| (_)_   _ _ __ ___  
