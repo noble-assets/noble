360d08d5aaf218a71f96bf8b1d9f28a697aa82bc
John Letey <john@noble.xyz>
2025-02-12 10:02:27 +0100

    chore: revert local script changes

--- local.sh
+++ local.sh
@@ -25,14 +25,13 @@ if ! [ -f .duke/data/priv_validator_state.json ]; then
   touch $TEMP && jq '.app_state.bank.denom_metadata += [{ "description": "Ondo US Dollar Yield", "denom_units": [{ "denom": "ausdy", "exponent": 0, "aliases": ["attousdy"] }, { "denom": "usdy", "exponent": 18 }], "base": "ausdy", "display": "usdy", "name": "Ondo US Dollar Yield", "symbol": "USDY" }]' .duke/config/genesis.json > $TEMP && mv $TEMP .duke/config/genesis.json
   touch $TEMP && jq '.app_state.bank.denom_metadata += [{ "description": "Hashnote US Yield Coin", "denom_units": [{ "denom": "uusyc", "exponent": 0, "aliases": ["microusyc"] }, { "denom": "usyc", "exponent": 6 }], "base": "uusyc", "display": "usyc", "name": "Hashnote US Yield Coin", "symbol": "USYC" }]' .duke/config/genesis.json > $TEMP && mv $TEMP .duke/config/genesis.json
   touch $TEMP && jq '.app_state.bank.denom_metadata += [{ "description": "Monerium EUR emoney", "denom_units": [{ "denom": "ueure", "exponent": 0, "aliases": ["microeure"] }, { "denom": "eure", "exponent": 6 }], "base": "ueure", "display": "eure", "name": "Monerium EUR emoney", "symbol": "EURe" }]' .duke/config/genesis.json > $TEMP && mv $TEMP .duke/config/genesis.json
-  touch $TEMP && jq '.app_state.dollar.portal.peers = { "10002": { "transceiver": "AAAAAAAAAAAAAAAAKcvx4HFm0xRGMHrgeZn6bRYiOZA=", "manager": "AAAAAAAAAAAAAAAAG3rhlLIMVVudmZyDX3TNzjamenQ=" } }' .duke/config/genesis.json > $TEMP && mv $TEMP .duke/config/genesis.json
   touch $TEMP && jq '.app_state."fiat-tokenfactory".mintingDenom = { "denom": "uusdc" }' .duke/config/genesis.json > $TEMP && mv $TEMP .duke/config/genesis.json
   touch $TEMP && jq '.app_state."fiat-tokenfactory".paused.paused = false' .duke/config/genesis.json > $TEMP && mv $TEMP .duke/config/genesis.json
   touch $TEMP && jq '.app_state.staking.params.bond_denom = "ustake"' .duke/config/genesis.json > $TEMP && mv $TEMP .duke/config/genesis.json
   touch $TEMP && jq '.app_state.wormhole.config.chain_id = 4009' .duke/config/genesis.json > $TEMP && mv $TEMP .duke/config/genesis.json
   touch $TEMP && jq '.app_state.wormhole.config.gov_chain = 1' .duke/config/genesis.json > $TEMP && mv $TEMP .duke/config/genesis.json
   touch $TEMP && jq '.app_state.wormhole.config.gov_address = "AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAQ="' .duke/config/genesis.json > $TEMP && mv $TEMP .duke/config/genesis.json
-  touch $TEMP && jq '.app_state.wormhole.guardian_sets = {"0":{"addresses":["E5R71IsY5T/a7ud/NHM5Gscnxjg="],"expiration_time":0}}' .duke/config/genesis.json > $TEMP && mv $TEMP .duke/config/genesis.json
+  touch $TEMP && jq '.app_state.wormhole.guardian_sets = {"0":{"addresses":["vvpCnVfNGLf4pNkaLamrSvBdD74="],"expiration_time":0}}' .duke/config/genesis.json > $TEMP && mv $TEMP .duke/config/genesis.json
 
   nobled genesis gentx validator 1000000ustake --chain-id "duke-1" --home .duke --keyring-backend test &> /dev/null
   nobled genesis collect-gentxs --home .duke &> /dev/null
