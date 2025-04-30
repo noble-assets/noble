55b8a961d5cbaf8add07c7534a75e0f9e06b2ea9
John Letey <john@noble.xyz>
2025-04-30 12:43:53 +0200

    fix: use registered upgrade name

--- upgrade/constants.go
+++ upgrade/constants.go
@@ -17,7 +17,7 @@
 package upgrade
 
 // UpgradeName is the name of this specific software upgrade used on-chain.
-const UpgradeName = "stratum"
+const UpgradeName = "stratrum"
 
 // UpgradeASCII is the ASCII art shown to node operators upon successful upgrade.
 const UpgradeASCII = `
