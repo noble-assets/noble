b3843d412a038cb831c6101a450d70749dbf6e86
John Letey <john@noble.xyz>
2025-04-30 18:07:37 +0200

    chore: revert upgrade name change (#543)

--- upgrade/constants.go
+++ upgrade/constants.go
@@ -17,7 +17,7 @@
 package upgrade
 
 // UpgradeName is the name of this specific software upgrade used on-chain.
-const UpgradeName = "stratrum"
+const UpgradeName = "stratum"
 
 // UpgradeASCII is the ASCII art shown to node operators upon successful upgrade.
 const UpgradeASCII = `
