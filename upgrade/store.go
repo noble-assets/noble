e7ff6d6a1e10fea38b888bde19bca0f4bbf9afdf
John Letey <john@noble.xyz>
2025-05-16 17:37:24 +0200

    chore: prepare `v10.0.0-rc.0` upgrade handler (#545)

--- upgrade/store.go
+++ upgrade/store.go
@@ -19,8 +19,6 @@ package upgrade
 import (
 	storetypes "cosmossdk.io/store/types"
 	upgradetypes "cosmossdk.io/x/upgrade/types"
-	hyperlanetypes "github.com/bcp-innovations/hyperlane-cosmos/x/core/types"
-	warptypes "github.com/bcp-innovations/hyperlane-cosmos/x/warp/types"
 	"github.com/cosmos/cosmos-sdk/baseapp"
 	ratelimittypes "github.com/cosmos/ibc-apps/modules/rate-limiting/v8/types"
 )
@@ -28,11 +26,12 @@ import (
 func CreateStoreLoader(upgradeHeight int64) baseapp.StoreLoader {
 	storeUpgrades := storetypes.StoreUpgrades{
 		Added: []string{
-			// Hyperlane Modules
-			hyperlanetypes.ModuleName,
-			warptypes.ModuleName,
 			// IBC Modules
 			ratelimittypes.ModuleName,
+		},
+		Deleted: []string{
+			// Noble Modules
+			"autocctp",
 		},
 	}
 
