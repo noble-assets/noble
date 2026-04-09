9d5717682e9d537b59b11d7c70036acdb929f19d
Luca Graziotti <luca@noble.xyz>
2026-04-09 11:15:45 +0200

    test: update upgrade test

--- e2e/upgrade_test.go
+++ e2e/upgrade_test.go
@@ -32,7 +32,7 @@ func TestChainUpgrade(t *testing.T) {
 	upgrades := []e2e.ChainUpgrade{
 		{
 			Image:       e2e.LocalImages[0],
-			UpgradeName: "v11.3.0",
+			UpgradeName: "v11.4.0",
 		},
 	}
 
